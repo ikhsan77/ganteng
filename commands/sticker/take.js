d1345985c426e203c1dae0c968e78edd3bfa7830
ikhsan77 <fajarkhairul723@gmail.com>
2022-11-18 11:07:48 +0700

    take

--- commands/sticker/take.js
+++ commands/sticker/take.js
@@ -4,6 +4,7 @@ const i18n = require('i18n')
 const axios = require('axios').default
 const { UploadFileUgu } = require('@libs/converter/upload')
 const fs = require('fs')
+const { apikey } = require('@config')
 
 /**
  * @type { ICommand }
@@ -32,9 +33,10 @@ module.exports = {
                 let media = 'database/src/shanndev.webp'
                 fs.writeFileSync(media, file)
                 let url = await UploadFileUgu(media)
+                let { data } = await axios.get('https://api.lolhuman.xyz/api/convert/webptomp4?apikey={apikey}&img={url}'.format({ apikey, url: url.url }))
 
                 fs.unlinkSync(media)
-                let res = await axios({ method: 'get', url: url.url, headers: { 'DNT': 1, 'Upgrade-Insecure-Request': 1, }, responseType: 'arraybuffer' })
+                let res = await axios({ method: 'get', url: data.result, headers: { 'DNT': 1, 'Upgrade-Insecure-Request': 1, }, responseType: 'arraybuffer' })
 
                 let buffer = await writeExifVid(res.data, { packname: m1, author: m2 })
                 if (!buffer) return msg.reply('Server dalam perbaikkan')
