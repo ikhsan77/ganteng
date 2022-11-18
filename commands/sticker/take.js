cec6f93b1f4caebaa7a1ea32f9a105fd373bced1
ikhsan77 <fajarkhairul723@gmail.com>
2022-11-18 11:00:22 +0700

    take

--- commands/sticker/take.js
+++ commands/sticker/take.js
@@ -1,6 +1,9 @@
 const { ICommand } = require('@libs/builders/command')
 const { writeExifImg, writeExifVid } = require('@libs/converter/exif')
 const i18n = require('i18n')
+const axios = require('axios').default
+const { UploadFileUgu } = require('@libs/converter/upload')
+const { up } = require('@database/migrations/20220822141103_users')
 
 /**
  * @type { ICommand }
@@ -26,7 +29,10 @@ module.exports = {
 
                 client.sendMessage(msg.from, { sticker: { url: buffer } }, { quoted: message }).catch(() => { return msg.reply('Terjadi kesalahan') })
             } else if (msg.quoted.message.stickerMessage.isAnimated) {
-                let buffer = await writeExifVid(file, { packname: m1, author: m2 })
+                let url = await UploadFileUgu(file)
+                let res = await axios({ method: 'get', url: url.url, headers: { 'DNT': 1, 'Upgrade-Insecure-Request': 1, }, responseType: 'arraybuffer' })
+
+                let buffer = await writeExifVid(res.data, { packname: m1, author: m2 })
                 if (!buffer) return msg.reply('Server dalam perbaikkan')
 
                 client.sendMessage(msg.from, { sticker: { url: buffer } }, { quoted: message }).catch(() => { return msg.reply('Terjadi kesalahan') })
