fcac3de12da9124616b115a71fec4ed908302961
ikhsan77 <fajarkhairul723@gmail.com>
2022-12-01 15:17:24 +0700

    yahoo

--- commands/sticker/sticker.js
+++ commands/sticker/sticker.js
@@ -10,16 +10,17 @@ module.exports = {
     category: 'Sticker',
     description: 'Sticker Maker',
     waitMessage: true,
-    callback: async ({ msg, client, message, prefix, command }) => {
-        const file = (await msg.download('buffer')) || (msg.quoted && (await msg.quoted.download('buffer')))
+    callback: async ({ msg, client, message }) => {
         if (msg.typeCheck.isImage || msg.typeCheck.isQuotedImage) {
+            let file = (await msg.download('buffer')) || (msg.quoted && (await msg.quoted.download('buffer')))
             let buffer = await writeExifImg(file, { packname: 'Fajarara', author: '@shannbot.ofc' })
+
             await client.sendMessage(msg.from, { sticker: { url: buffer } }, { quoted: message })
         } else if (msg.typeCheck.isVideo || msg.typeCheck.isQuotedVideo) {
+            let file = (await msg.download('buffer')) || (msg.quoted && (await msg.quoted.download('buffer')))
             let buffer = await writeExifVid(file, { packname: 'Fajarara', author: '@shannbot.ofc' })
+
             await client.sendMessage(msg.from, { sticker: { url: buffer } }, { quoted: message })
-        } else {
-            msg.reply(i18n.__('sticker.no_media'))
-        }
+        } else return msg.reply(i18n.__('sticker.no_media'))
     },
 }
