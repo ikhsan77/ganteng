dc4562750d7abd68c00037153aa594e004f261a8
ikhsan77 <fajarkhairul723@gmail.com>
2022-12-01 15:35:42 +0700

    yahoo

--- commands/convert/audio/audio-bass.js
+++ commands/convert/audio/audio-bass.js
@@ -11,24 +11,26 @@ module.exports = {
     description: '-',
     waitMessage: true,
     callback: async ({ msg, client, message }) => {
-        let media = (await msg.download('buffer')) || (msg.quoted && (await msg.quoted.download('buffer')))
-        let type = await fileType.fromBuffer(media)
-        let path = `shanndev.${type.ext}`
-        let path2 = 'shanndev.mp3'
-        let set = '-af equalizer=f=54:width_type=o:width=2:g=20'
-
         if (msg.typeCheck.isAudio || msg.typeCheck.isQuotedAudio) {
+            let media = (await msg.download('buffer')) || (msg.quoted && (await msg.quoted.download('buffer')))
+            let type = await fileType.fromBuffer(media)
+            let path = `shanndev.${type.ext}`
+            let path2 = 'shanndev.mp3'
+
             fs.writeFileSync(path, media)
 
-            exec(`ffmpeg -i ${path} ${set} ${path2}`, (err, stderr, stdout) => {
-                fs.unlinkSync(path)
-                if (err) return msg.reply('server dalam perbaikkan')
+            // exec(`ffmpeg -i ${path} '-af equalizer=f=54:width_type=o:width=2:g=20' ${path2}`, (err, stderr, stdout) => {
+            //     fs.unlinkSync(path)
+            //     if (err) return msg.reply('server dalam perbaikkan')
 
-                let buff = fs.readFileSync(path2)
-                msg.replyAudio(buff)
+            //     let buff = fs.readFileSync(path2)
+            //     msg.replyAudio(buff)
 
-                fs.unlinkSync(path2)
-            })
+            //     fs.unlinkSync(path2)
+            // })
+
+            let result = await exec(`ffmpeg -i ${path} '-af equalizer=f=54:width_type=o:width=2:g=20' ${path2}`)
+            console.log(result)
         } else return msg.reply('reply audio dengan caption #bass')
     }
 }
